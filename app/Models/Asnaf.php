4a6950f90e31a1a5e334281c13f031f3ee2dae8a
Ikiasa <96094148+Ikiasa@users.noreply.github.com>
2026-03-10 21:45:07 +0700

    fix: disable softdeletes on asnaf model to resolve production dashboard crash

--- app/Models/Asnaf.php
+++ app/Models/Asnaf.php
@@ -3,14 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+// use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Builder;
 
 class Asnaf extends Model
 {
-    use SoftDeletes;
+    // use SoftDeletes;
 
     protected $table = 'asnaf';
 
